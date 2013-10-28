ac5cfb886b6199e334fc4f6b834a93084536a8d5
Sylvain Hamann <sylvain@asiance.com>
2013-10-28 17:37:31 +0900

    Update angular-facebook.js

--- angular-facebook.js
+++ angular-facebook.js
@@ -1,9 +1,3 @@
-/*
- * bower-angular-facebook
- * (c) 2013 Laurent Le Graverend http://www.legraverend.fr
- * License: MIT
- */
-
 (function() {
   'use strict';
   
@@ -86,7 +80,60 @@
         return FB.api("/me/permissions", "DELETE", function(response) {
           return $rootScope.$broadcast("fb_get_login_status");
         });
+      },
+      addFeed: function(param) {
+        return FB.ui({ method:'feed', app_id:this.appId, redirect_ur:param.redirect_ur, from:param.from, to:param.to, link:param.link, picture:param.picture, source:param.source, name:param.name, caption:param.caption, description:param.description, ref:param.ref}, function(response){
+          if (response != null) {
+            return $rootScope.$broadcast("fb_post_feed_success",{
+              postID: response.post_id
+            });
+          }
+        });
+      },
+
+      addPhoto: function(img,tags) {
+        FB.api('me/photos','post',
+          {
+            message: 'I created an e-card for you !',
+            url: 'http://pokeballzcenter.webstarts.com/uploads/SquirtleSquad.jpg'
+          },
+          function(response) {
+            if (!response || response.error) {
+              console.log('Photo post failed');
+              console.log(response);
+            } else {
+              console.log('Photo post success');
+              var postId = response.id;
+              FB.api(postId+'/tags?tags='+JSON.stringify(tags), 'post', function(response){
+                if (!response || response.error) {
+                  console.log(response);
+                } else {
+                  console.log('Tag post success');
+                  return $rootScope.$broadcast("fb_post_photo_success",{
+                    data: response
+                  });
+                }
+              });
+            }
+          }
+        );
+      },
+
+      getFriends: function() {
+        FB.api('/me/friends',
+          function(response) {
+            if (!response || response.error) {
+              console.log('Get friends failed');
+            } else {
+              console.log('Get friends success');
+              return $rootScope.$broadcast("fb_get_friends_success",{
+                friends: response.data
+              });
+            }
+          }
+        );
       }
+
     };
   });
 
